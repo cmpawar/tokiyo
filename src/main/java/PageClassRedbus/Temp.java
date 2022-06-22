88108a253e555c9a587a0a16203062759b51372e
Chaitanya <Chaitanya@DESKTOP-NKOCCTH>
2022-06-22 22:45:07 +0530

    tokiyo check 011

--- src/main/java/PageClassRedbus/Temp.java
+++ src/main/java/PageClassRedbus/Temp.java
@@ -1,38 +1,19 @@
 package PageClassRedbus;
 
-import java.awt.geom.Arc2D.Float;
-import java.util.Scanner;
-
-import org.openqa.selenium.WebDriver;
-
 public class Temp {
-
-	static WebDriver driver;
+	
 	
 	public static void main(String[] args) {
-		
-		float hours;
-		float minutes;
-		float seconds;
-		
-		
-		Scanner sc = new Scanner(System.in);
-		float time =sc.nextFloat();
-		
-		hours = time;
-		minutes = hours*60;
-		seconds = minutes*60;
-		
-		
-		System.out.println(hours);
-		System.out.println(minutes);
-		System.out.println(seconds);
 	
-	
-	
-	
+
+		String str= "My name is Atos";
+		String b;
+		String s2="Chaitanya";
+		
+		//b = str.subSequence(0, 11)+ s2;
+		b= str.substring(0, 11)+s2;
+		System.out.println(b);
 	}
-	}
-		
-	
+
+}
 
---
+++ src/main/java/PageClassRedbus/temp02.java
@@ -0,0 +1,10 @@
+package PageClassRedbus;
+
+public class temp02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
